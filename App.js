a8f5d5386f10263921fea980708850a281c1cecf
lets-rush <arushsharma5005@gmail.com>
2025-08-17 13:33:37 +0530

    # working on parcel babel and understanding jsx

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World From, React"
-);
-console.log("heading", heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import { createRoot } from "react-dom/client";
 
-const paret = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "child1" }, "I am an h1 tag"),
-    React.createElement("h2", { id: "child2" }, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "child1" }, "I am an h1 tag"),
-    React.createElement("h2", { id: "child2" }, "I am an h2 tag"),
-  ]),
-]);
+const Heading = (<h1>React Jsx element</h1>);
+const root = createRoot(document.getElementById("root"));
+root.render(<Heading />);
+// Jsx when used, the function and the javascript that is used is put after data sanitization only
+// React element is a java script object which is when rendered is called the HTML element
+// Parcel transpiles jsx into js [behind the scenes babel does that for the parcel]
 
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(paret);
+const Parent = () => {
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div className="child">
+        <h2>Child Component</h2>
+        <div className="grandchild">
+          <h3>Grandchild Component</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+const root1 = createRoot(document.getElementById("root1"));
+root1.render(<Parent />);
